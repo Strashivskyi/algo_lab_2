c16ac9f71dd83f669a0b1f577da7d9e88761c00d
Strashivskyi <55955717+Strashivskyi@users.noreply.github.com>
2020-11-21 09:47:00 +0200

    added updated files for lab 2

---
+++ algo_lab_2-main/unittests.py
@@ -0,0 +1,33 @@
+import unittest
+
+from linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_add(self):
+        test_list = LinkedList()
+        test_list.add(8, None)
+        self.assertEqual(test_list.head.value, 8)
+        test_list.add(20, None)
+        self.assertIsNotNone(test_list.head.next)
+        self.assertEqual(test_list.head.next.value, 20)
+
+    def test_sort(self):
+        test_list = LinkedList()
+        test_list.add(12, None)
+        test_list.add(7, None)
+        test_list.add(40, None)
+        test_list.add(20, None)
+        test_list.head = test_list.sort(test_list.head)
+        self.assertEqual(test_list.head.value, 7)
+        self.assertEqual(test_list.head.next.value, 12)
+        self.assertEqual(test_list.head.next.next.value, 20)
+        self.assertEqual(test_list.head.next.next.next.value, 40)
+
+    def test_main(self):
+        test_list = list()
+        for element in open('max_sequence_length_result.csv', 'r').readline().split():
+            test_list.append(element)
+        self.assertEqual('4', test_list[0])
+        self.assertEqual(1, len(test_list))
